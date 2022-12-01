5bf3ad3c66bd8f24c5b18fd603fe038addffd025
w0791155 <98964957+w0791155@users.noreply.github.com>
2022-12-01 01:02:22 -0500

    added new properties and migration

---
+++ PrabhjeetBooks.DataAccess/Migrations/20221130161826_AddNewValidationToProduct.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PrabhjeetBooks.DataAccess.Migrations
+{
+    public partial class AddNewValidationToProduct : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Price100",
+                table: "Products",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Price50",
+                table: "Products",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Price100",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Price50",
+                table: "Products");
+        }
+    }
+}
